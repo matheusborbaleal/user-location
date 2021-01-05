81d99d7ef8390d73ae305422bf7bd55c68a8c602
Matheus Leal <matheus.leal@mv.com.br>
2021-01-05 09:34:37 -0300

    deployment

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ app.use('/', serveStatic(path.join(__dirname, '/dist')));
 const port = process.env.PORT || 8080
 app.listen(port)
 
-console.log('Listening on port ' + port);
+console.log('Listening on port: ' + port);
